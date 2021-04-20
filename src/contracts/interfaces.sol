fe85708d052a269e6eedbadcc1cd6126da4a61da
ilinzweilin <alina@centrifuge.io>
2021-04-20 23:42:39 +0200

    add: investTranches & loanCycle rpc tests

--- src/contracts/interfaces.sol
+++ src/contracts/interfaces.sol
@@ -1,9 +1,81 @@
-interface AuthLike {
+pragma solidity >=0.5.15 <0.6.0;
+
+interface IAuth {
     function rely(address) external;
     function deny(address) external;
     function wards(address) external returns(uint);
 }
 
+interface IPoolAdmin {
+    function updateSeniorMember(address usr, uint256 validUntil) external;
+    function updateJuniorMember(address usr, uint256 validUntil) external;
+    function relyAdmin(address usr) external;
+}
+
+interface IAssessor {
+    function maxReserve() external returns (uint);
+    function totalBalance() external returns (uint);
+    function calcJuniorTokenPrice(uint nav_, uint) external view returns(uint);
+    function calcSeniorTokenPrice(uint nav_, uint) external view returns(uint);
+}
+
+interface INavFeed {
+    function approximatedNAV() external returns (uint);
+    function update(bytes32 nftID, uint value) external;
+    function update(bytes32 nftID, uint value, uint risk) external;
+    function file(bytes32 what, bytes32 nftID_, uint maturityDate_) external;
+    function ceiling(uint loan) external view returns(uint);
+}
+
+interface IOperator {
+    function supplyOrder(uint amount) external;
+    function disburse() external returns(uint payoutCurrencyAmount, uint payoutTokenAmount, uint remainingSupplyCurrency,  uint remainingRedeemToken);
+}
+
+contract IShelf {
+    function lock(uint loan) external;
+    function unlock(uint loan) external;
+    function issue(address registry, uint token) public returns (uint loan);
+    function close(uint loan) external;
+    function borrow(uint loan, uint wad) external;
+    function withdraw(uint loan, uint wad, address usr) external;
+    function repay(uint loan, uint wad) external;
+    function shelf(uint loan) public returns(address registry,uint256 tokenId,uint price,uint principal, uint initial);
+    function file(bytes32 what, uint loan, address registry, uint nft) external;
+}
+
+contract IPile  {
+    function debt(uint loan) public returns(uint);
+}
+
+interface IReserve {
+    function currencyAvailable() external returns(uint);
+}
+
+interface IClerk {
+    function debt() external view returns(uint);
+}
+
+interface ICoordinator {
+    function closeEpoch() external;
+    function executeEpoch() external;
+    function challengeTime() external returns (uint);
+}
+
 interface RootLike {
     function relyContract(address contractAddr, address addr) external;
+}
+
+interface ERC20Like {
+    function balanceOf(address) external view returns (uint256);
+    function transferFrom(address, address, uint) external returns (bool);
+    function mint(address, uint256) external;
+    function burn(address, uint256) external;
+    function totalSupply() external view returns (uint256);
+    function approve(address, uint) external;
+    function ceiling(uint loan) external view returns(uint);
+}
+
+interface IRegistry {
+   function issue(address) external returns (uint);
 }
--- src/contracts/rpc-tests.sol
+++ src/contracts/rpc-tests.sol
@@ -1,8 +1,9 @@
 pragma solidity >=0.5.15 <0.6.0;
 
-import "ds-test/test.sol";
+import "lib/ds-test/src/test.sol";
 import "./addresses.sol";
 import "./interfaces.sol";
+import "lib/tinlake-title/src/title.sol";
 
 contract Hevm {
     function warp(uint256) public;
@@ -11,17 +12,191 @@ contract Hevm {
 
 contract TinlakeRPCTests is DSTest, TinlakeAddresses {
     Hevm public hevm;
+    RootLike root;
+    IPoolAdmin admin;
+    IAssessor assessor;
+    IOperator junior;
+    IOperator senior;
+    ICoordinator coordinator;
+    INavFeed nav;
+    IShelf shelf;
+    IPile pile;
+    IReserve reserve;
+    IClerk clerk;
+    Title registry;
+    ERC20Like dai;
+    ERC20Like tin;
+    ERC20Like drop;
+
+    address self;
+    uint256 constant ONE = 10 ** 27;
+
+    function rdiv(uint x, uint y) public pure returns (uint z) {
+        require(y > 0, "division by zero");
+        z = safeAdd(safeMul(x, ONE), y / 2) / y;
+    }
+
+    function safeMul(uint x, uint y) public pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x, "safe-mul-failed");
+    }
+
+    function safeAdd(uint x, uint y) public pure returns (uint z) {
+        require((z = x + y) >= x, "safe-add-failed");
+    }
 
     function setUp() public {
+        self = address(this);
         hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+        root = RootLike(ROOT);
+        admin = IPoolAdmin(POOL_ADMIN);
+        nav = INavFeed(FEED);
+        shelf = IShelf(SHELF);
+        pile = IPile(PILE);
+        assessor = IAssessor(ASSESSOR);
+        senior = IOperator(SENIOR_OPERATOR);
+        junior = IOperator(JUNIOR_OPERATOR);
+        coordinator = ICoordinator(COORDINATOR);
+        reserve = IReserve(RESERVE);
+        clerk = IClerk(CLERK);
+        tin = ERC20Like(JUNIOR_TOKEN);
+        drop = ERC20Like(SENIOR_TOKEN);
+        dai = ERC20Like(DAI);
+        registry = new Title("TEST", "TEST");
 
         // cheat: give testContract permissions on root contract by overriding storage
         // storage slot for permissions => keccak256(key, mapslot) (mapslot = 0)
-        hevm.store(ROOT, keccak256(abi.encode(address(this), uint(0))), bytes32(uint(1)));
+       hevm.store(ROOT, keccak256(abi.encode(self, uint(0))), bytes32(uint(1)));
     }
 
-    function testBasicRelyReserve() public {
-        RootLike(ROOT).relyContract(RESERVE, address(this));
-        assertEq(AuthLike(RESERVE).wards(address(this)), 1);
+    function investTranches() public {
+        // pre invest state
+        uint preReserveDaiBalance = dai.balanceOf(RESERVE);
+        uint preMakerDebt = clerk.debt();
+       
+       // get admin super powers
+       root.relyContract(POOL_ADMIN, self);
+       // whitelist self for tin & drop
+       admin.relyAdmin(self);
+       admin.updateSeniorMember(self, uint(-1));
+       admin.updateJuniorMember(self, uint(-1));
+
+        // get super powers on DAI contract
+       hevm.store(DAI, keccak256(abi.encode(self, uint(0))), bytes32(uint(1)));
+       
+       // mint DAI
+       uint maxInvest = (assessor.maxReserve() - assessor.totalBalance()) / 2; // make sure investment amount does not brek max reserve
+       dai.mint(self, maxInvest);
+
+       uint seniorInvest = maxInvest / 2;
+       uint juniorInvest = maxInvest - seniorInvest;
+
+       // invest tranches
+       dai.approve(SENIOR_TRANCHE, seniorInvest); // invest senior
+       senior.supplyOrder(seniorInvest);
+
+       dai.approve(JUNIOR_TRANCHE, juniorInvest); // invest junior 
+       junior.supplyOrder(juniorInvest);
+    
+       // close epoch & disburse
+       hevm.warp(now + coordinator.challengeTime());
+       coordinator.closeEpoch();
+       senior.disburse();
+       junior.disburse();
+  
+       // calc expected token balances for tin & drop
+       uint tinPrice = assessor.calcJuniorTokenPrice(nav.approximatedNAV(), 0);
+       uint dropPrice = assessor.calcSeniorTokenPrice(nav.approximatedNAV(), 0);
+       uint tinExpected = rdiv(juniorInvest, tinPrice);
+       uint dropExpected = rdiv(seniorInvest, dropPrice);
+
+       // check correct tin & drop token received
+       assertEqWithTolarence(tin.balanceOf(self), tinExpected);
+       assertEqWithTolarence(drop.balanceOf(self), dropExpected);
+
+       uint wipeAmount = assertMakerDebtReduced(preMakerDebt, maxInvest); // calc wipe amount for maker
+       uint reserveIncrease = maxInvest - wipeAmount; // calc reserve increase
+       assertEqWithTolarence(assessor.totalBalance(), preReserveDaiBalance + reserveIncrease); // check reserve balance increased correctly 
+       assertEqWithTolarence(preMakerDebt - wipeAmount, clerk.debt()); // check maker debt reduced correctly 
+    }
+
+    function testLoanCycle() public {
+        investTranches();
+        uint preDaiBalance = dai.balanceOf(self);
+
+        // issue nft
+        uint tokenId = registry.issue(self);
+
+        // issue loan
+        uint loanId = shelf.issue(address(registry), tokenId);
+
+        // set nft value
+        root.relyContract(FEED, self);
+        bytes32 nftId = keccak256(abi.encodePacked(address(registry), tokenId));
+        uint reserveBalance = assessor.totalBalance();
+        emit log_named_uint("balance", reserveBalance);
+        emit log_named_uint("available", reserve.currencyAvailable());
+        uint nftPrice = reserveBalance / 2; // make sure nft/loan value smaller then reserve balance
+        emit log_named_uint("price", nftPrice);
+        nav.update(nftId, nftPrice, 0);
+        nav.file("maturityDate", nftId, uint(-1));
+
+        // lock nft
+        registry.setApprovalForAll(SHELF, true);
+        shelf.lock(loanId);
+        // get loan ceiling
+        uint ceiling = (nav.ceiling(loanId));
+        emit log_named_uint("ceiling", ceiling);
+        // borrow
+        shelf.borrow(loanId, ceiling);
+        // withdraw
+        shelf.withdraw(loanId, ceiling, self);
+
+        // assert currency received
+        assertEq(dai.balanceOf(self), preDaiBalance + ceiling);
+
+        // warp
+        // hevm.warp(now + 5 days);
+        // uint debt = pile.debt(loanId);
+        // dai.mint(debt, self);
+        // uint preReserveBalance = dai.balanceOf(self);
+        // approve currency
+        // shelf.repay(loanId, debt);
+        // assert reserve increase
+        // assert maker repaid
+
+    }
+
+    function testLoanCycleWithMaker() public {
+        // raise creditline if not done yet
+        // loan amount higher then reserve
+        // testLoanCycle
+    }
+
+    // helper
+    function assertHasPermissions(address con, address ward) public {
+        uint perm = IAuth(con).wards(ward);
+        assertEq(perm, 1);
+    }
+
+    function assertHasNoPermissions(address con, address ward) public {
+        uint perm = IAuth(con).wards(ward);
+        assertEq(perm, 0);
+    }
+
+    function assertMakerDebtReduced(uint preDebt, uint investmentAmount) public returns (uint) {
+        if (preDebt > 1) {
+            if (preDebt > investmentAmount) {
+                assertEq(clerk.debt(), (preDebt - investmentAmount));
+                return investmentAmount;
+            } else {
+                assert(clerk.debt() <= 1);
+                return preDebt;
+            }
+        }
+        return 0;
+    }
+
+    function assertEqWithTolarence(uint val1, uint val2) public {
+        assert((val1-2) <= val2 || val2 <= (val1+2));
     }
 }
